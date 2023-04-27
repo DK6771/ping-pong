d0df1882f94cb74377c7ea31da4b8291554b367b
DK6771 <131388090+DK6771@users.noreply.github.com>
2023-04-27 18:49:06 +0300

    Add files via upload
    1 version:start

---
+++ ping pong/game.py
@@ -0,0 +1,7 @@
+#region start
+from pygame import *
+from random import randint
+window = display.set_mode((700,500))
+display.set_caption()
+background = transform.scale(image.load(picture),(700, 500))
+#endregion
