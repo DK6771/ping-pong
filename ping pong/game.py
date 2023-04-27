2d207ac094e956060f01bb18b703e44a374cd337
DK6771 <131388090+DK6771@users.noreply.github.com>
2023-04-27 18:57:00 +0300

    Update game.py
    version2: почти сделаный конец

--- ping pong/game.py
+++ ping pong/game.py
@@ -5,3 +5,25 @@ window = display.set_mode((700,500))
 display.set_caption()
 background = transform.scale(image.load(picture),(700, 500))
 #endregion
+
+while game:
+    window.blit(background, (0, 0))
+
+        sprite1.update()
+        sprite1.reset()
+        sprite2.update()
+        sprite2.reset()
+
+    else:
+        if finish == 1:
+            text3.draw()
+        elif finish == 2:
+            text4.draw()
+
+    for e in event.get():
+                if e.type == QUIT:
+                    game = False
+
+    FPS = 60
+    clock.tick(FPS)
+    display.update()
